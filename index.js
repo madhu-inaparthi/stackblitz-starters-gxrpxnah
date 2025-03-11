12496753b53de370c5df9e1a4704208682b27313
madhu-software <inaparthi.madhukiran.s89@kalvium.community>
2025-03-11 11:46:56 +0530

    done

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
+require('dotenv').config();
+
 const express = require('express');
 const { resolve } = require('path');
 
 const app = express();
-const port = 3010;
+
+const port = process.env.PORT || 3010;
 
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
+});
+
+app.get('/admin', (req, res) => {
+  const isAdmin = process.env.IS_ADMIN === 'true'; 
+
+  if (isAdmin) {
+    res.json({ message: "Welcome, Admin!", data: ["Admin Data 1", "Admin Data 2"] });
+  } else {
+    res.json({ message: "Welcome, User!", data: ["User Data 1", "User Data 2"] });
+  }
 });
 
 app.listen(port, () => {
